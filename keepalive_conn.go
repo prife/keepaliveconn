1153ac926d66f4ed5d1b15009771008d53969e39
zhongkaizhu <zhongkaizhu@tencent.com>
2023-02-09 15:53:04 +0800

    Refine Copy to use pointer

--- keepalive_conn.go
+++ keepalive_conn.go
@@ -3,8 +3,6 @@ package keepaliveconn
 import (
 	"bytes"
 	"encoding/binary"
-	"errors"
-	"io"
 	"net"
 	"time"
 )
@@ -163,41 +161,4 @@ func (c *KeepaliveConn) SetReadDeadline(t time.Time) error {
 
 func (c *KeepaliveConn) SetWriteDeadline(t time.Time) error {
 	return c.c.SetReadDeadline(t)
-}
-
-func Copy(dst net.Conn, src KeepaliveConn) (written int64, err error) {
-	for {
-		buf := make([]byte, 32*1024)
-		src.SetReadDeadline(time.Now().Add(src.keepaliveInterval))
-		nr, er := src.Read(buf)
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw < 0 || nr < nw {
-				nw = 0
-				if ew == nil {
-					ew = errors.New("invalid write result")
-				}
-			}
-			written += int64(nw)
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = errors.New("short write")
-				break
-			}
-		}
-		if er != nil {
-			if nerr, ok := er.(net.Error); ok && nerr.Timeout() {
-				src.Heartbeat([]byte("heart beat"))
-				continue
-			}
-			if er != io.EOF {
-				err = er
-			}
-			break
-		}
-	}
-	return written, err
 }
